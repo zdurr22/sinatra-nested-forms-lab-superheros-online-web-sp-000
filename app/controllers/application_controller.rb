532d4461ef5b714172e7d1768577867bd1df3450
zdurr22 <zdurr22@gmail.com>
2021-04-08 22:54:54 -0400

    commit tree 63f5b1a7620e4596b713108d51808a61460d729b

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,25 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+        
+        erb :super_hero
+    end
 
+    post '/teams' do
+        @team_name = params[:team][:name]
+        @team_motto = params[:team][:motto]
+
+        @hero_name = []
+        @hero_power = []
+        @hero_bio = []
+
+        @team_members = params[:team][:members]
+        @team_members.each do |member|
+            @hero_name << member[:name]
+            @hero_power << member[:power]
+            @hero_bio << member[:biography]
+        end
+        erb :team
+    end
 end
